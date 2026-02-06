408cb96d2966303f0de2de07e35fcf728580f2b3
yomi <yinkom0708@gmail.com>
2026-02-06 12:24:38 +0900

    feat: add basic code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="box">
+        <h1 className="title">CSS</h1>
+        <div className="japanese">完全に理解した</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
